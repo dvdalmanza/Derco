a6cd53fd5d3d9e060ae4bf601639b01a249e8209
dvd.almanza@gmail.com <dvd.almanza@gmail.com>
2022-05-20 19:31:00 -0500

    Se agrega el proyecto

---
+++ src/api/dercoApi.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const baseURL = 'http://192.168.1.186:8080/api';
+
+const dercoApi = axios.create({ baseURL });
+
+dercoApi.interceptors.request.use(
+    async(config) => {
+        const token = await AsyncStorage.getItem('token');
+        if ( token ) {
+            config.headers['x-token'] = token;
+        }
+        return config;
+    }
+);
+
+export default dercoApi;
---
+++ src/hooks/useCategories.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import dercoApi from "../api/dercoApi";
+import { CagetoriesResponse, Categoria } from '../interfaces/appInterfaces';
+
+
+export const useCategories = () => {
+    
+    const [ isLoading, setIsLoading ] = useState( true )
+    const [ categories, setCategories ] = useState<Categoria[]>([]);
+    
+    useEffect(() => {
+        getCategories();
+    }, [])
+
+
+    const getCategories = async() => {
+        const resp = await dercoApi.get<CagetoriesResponse>('/categorias');
+        setCategories( resp.data.categorias );
+        setIsLoading(false);
+    }
+
+
+    return {
+        isLoading,
+        categories
+    }
+}
